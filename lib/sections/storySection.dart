ccacd5eac5489dca9a3b7602c59505c4ab77f959
Siljo Denny <siljodennyjohn>
2024-12-17 01:06:14 +0530

    Updted storysection

--- lib/sections/storySection.dart
+++ lib/sections/storySection.dart
@@ -8,50 +8,57 @@ class StorySection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 250,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
+      child: Row(
         children: [
-          StoryCard(
-            labelText: "Add your Story",
-            story: 'assets/avatars/prithviraj.jpg',
-            avatarStory: 'assets/avatars/prithviraj.jpg',
-            createStoryStatus: true,
-          ),
-          StoryCard(
-            labelText: "Mohanlal",
-            story: 'assets/story/mohanlal.jpg',
-            avatarStory: 'assets/avatars/mohanlal.jpg',
-            celebrityStatus: true,
-          ),
-          StoryCard(
-            labelText: "Mammooty",
-            story: 'assets/story/mamooty.jpg',
-            avatarStory: 'assets/avatars/mamooty.jpg',
-            celebrityStatus: true,
-          ),
-          StoryCard(
-            labelText: "Tamanna",
-            story: 'assets/story/tamanna.jpg',
-            avatarStory: 'assets/avatars/thamanna.jpg',
-            celebrityStatus: true,
-          ),
-          StoryCard(
-            labelText: "Tovino Thomas",
-            story: 'assets/story/tovino.jpg',
-            avatarStory: 'assets/avatars/tovino.jpg',
-            celebrityStatus: true,
-          ),
-          StoryCard(
-            labelText: "Dulquer Salman",
-            story: 'assets/story/dulquer.jpg',
-            avatarStory: 'assets/avatars/dulquar.jpg',
-            celebrityStatus: true,
-          ),
-          StoryCard(
-            labelText: "Fahad Fassil",
-            story: 'assets/story/fahad.jpg',
-            avatarStory: 'assets/avatars/fahad.jpg',
-            celebrityStatus: true,
+          const StoryCard(
+                labelText: "Add your Story",
+                story: 'assets/avatars/prithviraj.jpg',
+                avatarStory: 'assets/avatars/prithviraj.jpg',
+                createStoryStatus: true,
+              ),
+              const SizedBox(width: 2),
+          Expanded(
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: const [
+                StoryCard(
+                  labelText: "Mohanlal",
+                  story: 'assets/story/mohanlal.jpg',
+                  avatarStory: 'assets/avatars/mohanlal.jpg',
+                  celebrityStatus: true,
+                ),
+                StoryCard(
+                  labelText: "Mammooty",
+                  story: 'assets/story/mamooty.jpg',
+                  avatarStory: 'assets/avatars/mamooty.jpg',
+                  celebrityStatus: true,
+                ),
+                StoryCard(
+                  labelText: "Tamanna",
+                  story: 'assets/story/tamanna.jpg',
+                  avatarStory: 'assets/avatars/thamanna.jpg',
+                  celebrityStatus: true,
+                ),
+                StoryCard(
+                  labelText: "Tovino Thomas",
+                  story: 'assets/story/tovino.jpg',
+                  avatarStory: 'assets/avatars/tovino.jpg',
+                  celebrityStatus: true,
+                ),
+                StoryCard(
+                  labelText: "Dulquer Salman",
+                  story: 'assets/story/dulquer.jpg',
+                  avatarStory: 'assets/avatars/dulquar.jpg',
+                  celebrityStatus: true,
+                ),
+                StoryCard(
+                  labelText: "Fahad Fassil",
+                  story: 'assets/story/fahad.jpg',
+                  avatarStory: 'assets/avatars/fahad.jpg',
+                  celebrityStatus: true,
+                ),
+              ],
+            ),
           ),
         ],
       ),
