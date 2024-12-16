152ce0ee01bb2b4ebfac9441cc96d030c9106230
Siljo Denny <siljodennyjohn>
2024-12-16 22:38:17 +0530

    remove debug banner

--- lib/home.dart
+++ lib/home.dart
@@ -17,6 +17,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text(
